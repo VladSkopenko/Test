514d149e994ff63e5646399b76cc3bc6c787d44c
VladSkopenko <skopirka2k17@gmail.com>
2024-02-28 18:52:29 +0300

    +pil

--- main.py
+++ main.py
@@ -17,3 +17,29 @@ class AuthorItem(Item):
     born_date = Field()
     born_location = Field()
     description = Field()
+
+
+class QuotesSpyder(scrapy.Spider):
+    name = "get_quotes"
+    allowed_domains = ["quotes.toscrape.com"]
+    start_urls = ["https://quotes.toscrape.com/"]
+    custom_settings = {}
+
+    def parse(self, response, **kwargs):
+        for q in response.xpath("/html//div[@class='quote']"):
+            quote = q.xpath("span[@class='text']/text()").get().strip()
+            author = q.xpath("span/small[@class='author']/text()").get().strip()
+            tags = q.xpath("div[@class='tags']/a/text()").extract()
+            # TODO: clear tags
+            yield QuoteItem(quote=quote, author=author, tags=tags)
+            yield response.follow(url=self.start_urls[0] + q.xpath("span/a/@href").get(), callback=self.parse_author)
+
+    def parse_author(self, response, **kwargs):
+        content = response.xpath("/html//dib[@class='author-details']")
+        fullname = content.xpath("h3[@class='author-title']/text()").get().strip()
+
+
+if __name__ == "__main__":
+    process = CrawlerProcess()
+    process.crawl(QuotesSpyder)
+    process.start()
