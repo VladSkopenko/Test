9a34d3a3c8c8d739fb6dd560afb8a7e4cd8d1ced
VladSkopenko <skopirka2k17@gmail.com>
2024-02-28 17:53:25 +0300

    start testing scrapy

---
+++ main.py
@@ -0,0 +1,19 @@
+import json
+
+import scrapy
+from itemadapter import ItemAdapter
+from scrapy.crawler import CrawlerProcess
+from scrapy.item import Item, Field
+
+
+class QuoteItem(Item):
+    quote = Field()
+    author = Field()
+    tags = Field()
+
+
+class AuthorItem(Item):
+    fullname = Field()
+    born_date = Field()
+    born_location = Field()
+    description = Field()
